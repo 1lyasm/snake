507bc47e8cbb02b671d86cd024e85f90935d3f60
1lyasm <1lyasm.dev@gmail.com>
2024-04-13 20:08:45 +0300

    move circle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
 #include "glad.h"
 #include <GLFW/glfw3.h>
+#include <chrono>
+#include <iostream>
+#include <math.h>
+#include <thread>
+
+#define STEPS 100
+#define ANGLE 3.1415926 * 2.f / STEPS
 
 int main() {
   GLFWwindow *window;
@@ -14,12 +20,27 @@ int main() {
     glfwTerminate();
     return -1;
   }
-  glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+  float x = 0, y = 0, radius = 1 / 20.0;
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
-
     glClear(GL_COLOR_BUFFER_BIT);
-
+    float oldx = x;
+    float oldy = y - radius;
+    for (int i = 0; i <= STEPS; ++i) {
+      float newx = x + radius * sin(ANGLE * i);
+      float newy = -radius * cos(ANGLE * i);
+      glBegin(GL_TRIANGLES);
+      glColor3f(1.0, 0.0, 0.0);
+      glVertex3f(x, 0.0f, 0.0f);
+      glVertex3f(oldx, oldy, 0.0f);
+      glVertex3f(newx, newy, 0.0f);
+      glEnd();
+      oldx = newx;
+      oldy = newy;
+    }
+    x += 2 * radius;
+    std::chrono::milliseconds span((int)1e3);
+    std::this_thread::sleep_for(span);
     glfwSwapBuffers(window);
   }
   glfwTerminate();
