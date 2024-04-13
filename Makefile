b2570986f6756dbd6d72b499f94893cde6e336eb
1lyasm <1lyasm.dev@gmail.com>
2024-04-13 19:22:48 +0300

    add glad

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 all:
-	clang++ -g -fsanitize=address -Weverything \
-		-lglfw -lGL -lm -lX11 -lpthread -lXrandr -ldl \
-		src/main.cpp -o main
+	clang -c -g -fsanitize=address -Wall src/glad.c -o src/glad.o
+	clang++ -c -g -fsanitize=address -Wall \
+		src/main.cpp -o src/main.o
+	clang++ -fsanitize=address -lglfw -lGL -lm -lX11 -lpthread -lXrandr -ldl \
+		src/*.o -o src/main
 run:
-	./main
+	./src/main
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include "glad.h"
 #include <GLFW/glfw3.h>
 
 int main() {
-    GLFWwindow *window;
-    if (!glfwInit()) {
-        return -1;
-    }
-    window = glfwCreateWindow(640, 480, "Window!", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
-        glfwSwapBuffers(window);
-    }
+  GLFWwindow *window;
+  if (!glfwInit()) {
+    return -1;
+  }
+  window = glfwCreateWindow(640, 480, "window", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "could not load opengl\n";
     glfwTerminate();
+    return -1;
+  }
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    glfwSwapBuffers(window);
+  }
+  glfwTerminate();
 }
